58babd0e4009ba017930861a346319ad2bcc0bc6
jiyounkim <wldus021026@naver.com>
2025-01-22 16:52:35 +0900

    settings.py debug=true로 수정

--- ai_interview/settings.py
+++ ai_interview/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Django 설정
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = config('DEBUG',default=False, cast=bool)
+DEBUG = True
+# DEBUG = config('DEBUG',default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 

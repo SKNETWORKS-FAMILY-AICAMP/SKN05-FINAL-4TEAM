82342bfb5ac2671b65aef21bace148f76db6a579
yoonkwan <yoonkwan95@gmail.com>
2025-01-23 09:40:27 +0900

    데이터 파싱 작업 진행 중

--- interview/utils.py
+++ interview/utils.py
@@ -4,13 +4,21 @@ def call_whisper(audio_file_path):
     """
     Whisper 모델을 호출하여 음성 파일을 텍스트로 변환
     """
-    url = "http://213.173.110.107:17709"  # RUNPOD Whisper API URL
+    # Runpod Whisper API URL
+    url = "https://api.runpod.ai/v2/kyj33jbol8vbm2/run"
 
-    with open(audio_file_path, 'rb') as audio:
-        files = {'audio': audio}
-        response = requests.post(url, files=files)
+    # 파일 업로드
+    with open(audio_file_path, "rb") as audio_file:
+        payload = {
+            "audio": audio_file
+            "model": "medium",
+            "transcription": "formatted_text",
+            "language": "ko"
+            }
+        response = requests.post(url, json=payload)
 
+    # 응답 처리
     if response.status_code == 200:
-        return response.json().get('text')
+        return response.json().get("text")  # 텍스트 결과 반환
     else:
         return f"Error: {response.status_code} - {response.text}"

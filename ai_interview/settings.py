45b428cb55eeb698427f42f22a7874e755a51806
yoonkwan <yoonkwan95@gmail.com>
2025-01-23 16:26:14 +0900

    whisper endpoint 호출 구현 완료

--- ai_interview/settings.py
+++ ai_interview/settings.py
@@ -146,3 +146,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#RunPod 설정
+RUNPOD_API_KEY = config("RUNPOD_API_KEY", default="")
+STT_ENDPOINT = config("STT_endpoint", default="")
--- interview/utils.py
+++ interview/utils.py
@@ -1,24 +1,22 @@
-import requests
+import runpod
+from django.conf import settings
 
-def call_whisper(audio_file_path):
-    """
-    Whisper 모델을 호출하여 음성 파일을 텍스트로 변환
-    """
-    # Runpod Whisper API URL
-    url = "https://api.runpod.ai/v2/kyj33jbol8vbm2/run"
-
-    # 파일 업로드
-    with open(audio_file_path, "rb") as audio_file:
-        payload = {
-            "audio": audio_file
-            "model": "medium",
-            "transcription": "formatted_text",
-            "language": "ko"
-            }
-        response = requests.post(url, json=payload)
-
-    # 응답 처리
-    if response.status_code == 200:
-        return response.json().get("text")  # 텍스트 결과 반환
-    else:
-        return f"Error: {response.status_code} - {response.text}"
+def whisper_call(audio_path:str):
+    runpod.api_key = settings.RUNPOD_API_KEY
+    whisper_endpoint = runpod.Endpoint(settings.STT_ENDPOINT)
+    try:
+        run_request = whisper_endpoint.run_sync(
+            {
+                "input": {
+                    "audio": f"{audio_path}",
+                    "model": "medium",
+                    "transcription": "formatted_text",
+                    "language": "ko"
+                    }
+        }
+    )
+        return run_request
+    
+    except Exception as e:
+        print(f"에러 유형: {e.__class__.__name__}")
+        print(f"에러 메시지: {e}")
